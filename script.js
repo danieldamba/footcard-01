8248152c3395a83d641a42e540b00c26eb5e57e4
Daniel Damba <dann1601007@gmail.com>
2026-07-11 20:56:50 +0200

    Add data for handling player infos generation

---
+++ script.js
@@ -0,0 +1,33 @@
+
+let firstNames = [
+    "Amina", "Santiago", "Mei", "Olga", "Kwame", "Priya",
+    "Noor", "Diego", "Yuki", "Chloe", "Amara", "Liam",
+    "Nia", "Hana", "Mateo", "Anaya", "Jamal", "Lena",
+    "Sofia", "Finn", "Imani", "Aria", "Kai", "Zara",
+    "Maya", "Lucas", "Nina", "Ravi", "Aya", "Jonah"
+];
+
+let lastNames = [
+    "Garcia", "Kim", "Patel", "Smith", "Nguyen", "Johnson",
+    "Chen", "Martinez", "Ali", "Brown", "Singh", "Lopez",
+    "Hassan", "Davis", "Rodriguez", "Khan", "Miller", "Gonzalez",
+    "Ahmed", "Wilson", "Santos", "Taylor", "Hernandez", "Anderson",
+    "Park", "Thomas", "Ramirez", "White", "Murphy", "Young"
+];
+
+let countries = [
+    `England`, `Switzerland`, `France`, `Kosovo`,
+    `Brazil`, `Colombia`, `Puerto Rico`, `USA`,
+    `DR Congo`, `Libya`, `Kenya`, `Rwanda`,
+    `Japan`, `Korea`, `Iran`, `Russia`
+]
+
+let positions = [
+    `GK`, `FB`, `CB`,
+    `DM`, `CM`, `AM`,
+    `AI`, `BU`
+];
+
+let prefFoot = [
+    `R`, `L`, `Ambidexter`
+];
